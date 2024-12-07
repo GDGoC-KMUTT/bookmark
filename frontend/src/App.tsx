6895a32c124daa09ea730ce8c80852ff820da165
Eve-nichakon <hello.eve555@gmail.com>
2024-12-07 23:10:35 +0700

    add Source Code Pro font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { router } from "./configs/routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { router } from "./configs/routes"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {router.map((item) => (
-          <Route key={item.path} element={item.element} path={item.path} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                {router.map((item) => (
+                    <Route key={item.path} element={item.element} path={item.path} />
+                ))}
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
+
