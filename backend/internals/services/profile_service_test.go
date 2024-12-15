e7a1707dbf785f949cd2cac828fe2ad7a7ffd2b4
Eve-nichakon <hello.eve555@gmail.com>
2024-12-15 16:52:49 +0700

    add profile unit test

--- backend/internals/services/profile_service_test.go
+++ backend/internals/services/profile_service_test.go
@@ -6,7 +6,9 @@ import (
 	"backend/internals/services"
 	"backend/internals/utils"
 	mockRepositories "backend/mocks/repositories"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"strconv"
 	"testing"
@@ -21,13 +23,15 @@ func (suite *ProfileTestSuit) TestGetUserInfoWhenSuccess() {
 	// Arrange
 	mockUserRepo := new(mockRepositories.UserRepository)
 
+	// Mock data
 	mockUserId := utils.Ptr[uint64](1)
 	mockFirstName := utils.Ptr("testfn")
 	mockLastName := utils.Ptr("testln")
 	mockEmail := utils.Ptr("test@gmail.com")
 	mockPhotoUrl := utils.Ptr("url")
 
-	mockUserRepo.EXPECT().FindUserByID(mockUserId).Return(&models.User{
+	// mock user repo
+	mockUserRepo.EXPECT().FindUserByID(mock.Anything).Return(&models.User{
 		Id:        mockUserId,
 		Firstname: mockFirstName,
 		Lastname:  mockLastName,
@@ -35,14 +39,36 @@ func (suite *ProfileTestSuit) TestGetUserInfoWhenSuccess() {
 		PhotoUrl:  mockPhotoUrl,
 	}, nil)
 
-	underTest := services.NewLoginService(mockUserRepo)
+	// Test
+	underTest := services.NewProfileService(mockUserRepo)
 
 	// Test Success
-	convList, err := underTest.GetUserInfo(strconv.Itoa(int(*mockUserId)))
+	userInfo, err := underTest.GetUserInfo(utils.Ptr(strconv.Itoa(int(*mockUserId))))
 
-	is.Equal(&mockTopicId, convList[0].ID)
-	is.Equal("test1", *convList[0].Topic)
+	is.Equal(*mockUserId, *userInfo.Id)
+	is.Equal(*mockFirstName, *userInfo.Firstname)
 	is.NoError(err)
+}
+
+func (suite *ProfileTestSuit) TestGetUserInfoWhenFailed() {
+	is := assert.New(suite.T())
+	// Arrange
+	mockUserRepo := new(mockRepositories.UserRepository)
+
+	// Mock data
+	mockUserId := utils.Ptr[uint64](1)
+
+	// mock user repo
+	mockUserRepo.EXPECT().FindUserByID(mock.Anything).Return(nil, fmt.Errorf("user not found"))
+
+	// Test
+	underTest := services.NewProfileService(mockUserRepo)
+
+	// Test Success
+	userInfo, err := underTest.GetUserInfo(utils.Ptr(strconv.Itoa(int(*mockUserId))))
+
+	is.ErrorIs(err, fmt.Errorf("user not found"))
+	is.Nil(userInfo)
 }
 
 func TestProfileService(t *testing.T) {
