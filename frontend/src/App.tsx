3adbff2f0d642a602270a3d8a798772f9dc4253b
Eve-nichakon <hello.eve555@gmail.com>
2024-12-15 14:19:34 +0700

    draft navbar+footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { router } from "./configs/routes"
 import ToastProvider from "./configs/toast"
+import Navbar from "@/components/navbar"
+import Footer from "./components/footer"
 
 function App() {
     return (
         <ToastProvider>
+            <Navbar />
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Navigate to="/course" />} />
@@ -13,6 +16,7 @@ function App() {
                     ))}
                 </Routes>
             </BrowserRouter>
+            <Footer />
         </ToastProvider>
     )
 }
---
+++ frontend/src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="w-full bg-white h-[3rem] fixed top-0 shadow-md flex justify-between">
+            <div>menu</div>
+            <div>profile</div>
+        </div>
+    )
+}
+
+export default Navbar
+
