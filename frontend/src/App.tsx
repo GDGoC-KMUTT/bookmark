dfaa8e74b4e91bf5435aa0982a2742d4645d2acd
Eve-nichakon <hello.eve555@gmail.com>
2024-12-15 19:11:35 +0700

    update server.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { router } from "./configs/routes"
+import ToastProvider from "./configs/toast"
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/course" />} />
-                {router.map((item) => (
-                    <Route key={item.path} element={item.element} path={item.path} />
-                ))}
-            </Routes>
-        </BrowserRouter>
+        <ToastProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/course" />} />
+                    {router.map((item) => (
+                        <Route key={item.path} element={item.element} path={item.path} />
+                    ))}
+                </Routes>
+            </BrowserRouter>
+        </ToastProvider>
     )
 }
 
