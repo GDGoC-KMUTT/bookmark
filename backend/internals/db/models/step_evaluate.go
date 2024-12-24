6552f62e6f83a8811c3319d225276f9b1a3845f3
Eve-nichakon <hello.eve555@gmail.com>
2024-12-25 02:51:38 +0700

    update mockData.json

--- backend/internals/db/models/step_evaluate.go
+++ backend/internals/db/models/step_evaluate.go
@@ -4,10 +4,10 @@ import "time"
 
 type StepEvaluate struct {
 	Id          *uint64    `gorm:"primaryKey"`
-	StepId      *uint64    `gorm:"index:idx_step_evaluate,unique; not null"`
+	StepId      *uint64    `gorm:"index:idx_step_evaluate; not null"`
 	Step        *Step      `gorm:"foreignKey:StepId"`
 	Gem         *int       `gorm:"not null"`
-	Order       *int       `gorm:"index:idx_step_evaluate,unique; not null"`
+	Order       *int       `gorm:"index:idx_step_evaluate; not null"`
 	Question    *string    `gorm:"type:TEXT; not null"`
 	Type        *string    `gorm:"type:VARCHAR(255) CHECK(type IN ('check', 'text', 'image')); not null"`
 	Instruction *string    `gorm:"type:TEXT; not null"`
