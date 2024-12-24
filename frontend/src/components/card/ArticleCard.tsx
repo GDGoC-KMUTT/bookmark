06e903f6e2bcbd81c48a98a2c8a0a79492a87bac
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-12-24 18:58:33 +0700

    add ArticleCard

---
+++ frontend/src/components/card/ArticleCard.tsx
@@ -0,0 +1,25 @@
+import { PayloadArticle } from "@/api/api"
+import { Text } from "lucide-react"
+import { FC } from "react"
+
+interface IArticleCard {
+    article: PayloadArticle
+}
+
+const ArticleCard: FC<IArticleCard> = ({ article }) => {
+    return (
+        <div className="flex flex-col p-4 w-[380px] h-[200px] bg-background rounded-sm border border-border break-words m-2">
+            <div className="text-wrap">
+                <div className="flex items-center text-border">
+                    <Text className="stroke-2 w-5 h-5" />
+                    <p className="text-md font-normal">Article</p>
+                </div>
+
+                <h2 className="text-foreground text-xl">{article.title}</h2>
+            </div>
+        </div>
+    )
+}
+
+export default ArticleCard
+
