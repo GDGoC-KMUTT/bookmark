3d5771c62f06c64c6359539ba0e098e380ff0d79
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2024-12-25 23:55:03 +0700

    edit icon

--- frontend/src/components/coursecard/index.tsx
+++ frontend/src/components/coursecard/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Bookmark } from "lucide-react"
 
 type CourseCardProps = {
     courseName: string
@@ -12,20 +13,7 @@ const CourseCard: React.FC<CourseCardProps> = ({ courseName, fieldName, imageUrl
         <Link to={`/courses/${courseId}`} className="block">
             <div className="border p-5 pb-2 w-[350px] h-[200px] pr-10 rounded-sm flex flex-col space-y-2">
                 <div className="flex items-center space-x-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5 text-gray-500"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-                        />
-                    </svg>
+                <Bookmark className="stroke-2 w-5 h-5 text-gray-500" />
                     <p className="text-gray-500">{fieldName}</p>
                 </div>
                 <p className="font-medium pb-3 text-black break-words">{courseName}</p>
