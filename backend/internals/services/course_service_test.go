db89b14bcfb5b2ca88785d3544c0fbb45cea72de
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-12-25 21:53:46 +0700

    add GetCourseByFieldId and GetAllFieldTypes service test

--- backend/internals/services/course_service_test.go
+++ backend/internals/services/course_service_test.go
@@ -2,13 +2,15 @@ package services_test
 
 import (
 	"backend/internals/db/models"
-	"backend/internals/services"
 	"backend/internals/entities/payload"
+	"backend/internals/services"
+	"backend/internals/utils"
 	mockRepositories "backend/mocks/repositories"
 	"fmt"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 func PtrUint64(val uint64) *uint64 {
@@ -27,6 +29,7 @@ func (suite *CourseTestSuite) TestGetCurrentCourseSuccess() {
 	is := assert.New(suite.T())
 
 	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
 	mockUserID := uint(1)
 
 	mockCourse := models.Course{
@@ -38,7 +41,7 @@ func (suite *CourseTestSuite) TestGetCurrentCourseSuccess() {
 		GetCurrentCourse(mockUserID).
 		Return(&mockCourse, nil)
 
-	underTest := services.NewCourseService(mockCourseRepo)
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
 
 	course, err := underTest.GetCurrentCourse(mockUserID)
 
@@ -51,13 +54,15 @@ func (suite *CourseTestSuite) TestGetCurrentCourseFetchError() {
 	is := assert.New(suite.T())
 
 	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
 	mockUserID := uint(1)
 
 	mockCourseRepo.EXPECT().
 		GetCurrentCourse(mockUserID).
 		Return(nil, fmt.Errorf("failed to fetch current course"))
 
-	underTest := services.NewCourseService(mockCourseRepo)
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
 
 	course, err := underTest.GetCurrentCourse(mockUserID)
 
@@ -67,45 +72,144 @@ func (suite *CourseTestSuite) TestGetCurrentCourseFetchError() {
 }
 
 func (suite *CourseTestSuite) TestGetTotalStepsByCourseIdSuccess() {
-    is := assert.New(suite.T())
+	is := assert.New(suite.T())
 
-    mockCourseRepo := new(mockRepositories.CourseRepository)
-    mockCourseID := uint(10)
+	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
 
-    expectedPayload := &payload.TotalStepsByCourseIdPayload{
-        CourseId: mockCourseID,
-        TotalSteps: 3,
-    }
+	mockCourseID := uint(10)
 
-    mockCourseRepo.On("GetTotalStepsByCourseId", mockCourseID).Return(3, nil)
+	expectedPayload := &payload.TotalStepsByCourseIdPayload{
+		CourseId:   mockCourseID,
+		TotalSteps: 3,
+	}
 
-    underTest := services.NewCourseService(mockCourseRepo)
+	mockCourseRepo.On("GetTotalStepsByCourseId", mockCourseID).Return(3, nil)
 
-    actualPayload, err := underTest.GetTotalStepsByCourseId(mockCourseID)
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
 
-    is.NoError(err)
-    is.Equal(expectedPayload, actualPayload)
+	actualPayload, err := underTest.GetTotalStepsByCourseId(mockCourseID)
 
-    mockCourseRepo.AssertExpectations(suite.T())
+	is.NoError(err)
+	is.Equal(expectedPayload, actualPayload)
+
+	mockCourseRepo.AssertExpectations(suite.T())
 }
 
 func (suite *CourseTestSuite) TestGetTotalStepsByCourseIdFetchError() {
 	is := assert.New(suite.T())
 
 	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
 	mockCourseID := uint(10)
 
 	mockCourseRepo.EXPECT().
 		GetTotalStepsByCourseId(mockCourseID).
 		Return(0, fmt.Errorf("failed to fetch total steps"))
 
-	underTest := services.NewCourseService(mockCourseRepo)
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
 
 	payload, err := underTest.GetTotalStepsByCourseId(mockCourseID)
 
 	is.Error(err)
 	is.Equal("failed to fetch total steps", err.Error())
 	is.Nil(payload)
+}
+
+func (suite *CourseTestSuite) TestGetCourseByFieldIdWhenSuccess() {
+	is := assert.New(suite.T())
+
+	// Arrange
+	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
+	// Mock data
+	mockCourseId := utils.Ptr(uint64(1))
+	mockCourseName := utils.Ptr("testname")
+	mockCourseFieldId := utils.Ptr(uint(1))
+	mockFieldId := utils.Ptr(uint64(1))
+	mockFieldName := utils.Ptr("testname")
+	mockFieldImageUrl := utils.Ptr("testimageurl")
+
+	mockCourseRepo.EXPECT().FindCourseByFieldId(mockCourseFieldId).Return([]models.Course{{
+		Id:      mockCourseId,
+		Name:    mockCourseName,
+		FieldId: mockFieldId,
+	}}, &models.FieldType{
+		Id:       mockFieldId,
+		Name:     mockFieldName,
+		ImageUrl: mockFieldImageUrl,
+	}, nil)
+
+	// Test
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
+	courses, err := underTest.GetCourseByFieldId(mockCourseFieldId)
+	is.Equal(courses[0].Id, mockCourseId)
+	is.Equal(courses[0].Name, mockCourseName)
+	is.Equal(courses[0].FieldId, mockFieldId)
+	is.Equal(courses[0].FieldName, mockFieldName)
+	is.Equal(courses[0].FieldImageUrl, mockFieldImageUrl)
+	is.NoError(err)
+
+}
+func (suite *CourseTestSuite) TestGetCourseByFieldIdWhenFailed() {
+	is := assert.New(suite.T())
+
+	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
+	mockFieldId := utils.Ptr(uint(1))
+
+	mockCourseRepo.On("FindCourseByFieldId", mockFieldId).Return(nil, nil, fmt.Errorf("courses not found"))
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
+
+	courses, err := underTest.GetCourseByFieldId(mockFieldId)
+
+	is.Nil(courses)
+	is.NotNil(err)
+}
+
+func (suite *CourseTestSuite) TestGetAllFieldTypesWhenSuccess() {
+	is := assert.New(suite.T())
+
+	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
+	mockId := utils.Ptr(uint64(1))
+	mockName := utils.Ptr("testname")
+	mockImageUrl := utils.Ptr("testimageurl")
+
+	mockFieldTypeRepo.On("FindAllFieldTypes").Return([]models.FieldType{{Id: mockId,
+		Name:     mockName,
+		ImageUrl: mockImageUrl},
+	}, nil)
+
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
+
+	fieldTypes, err := underTest.GetAllFieldTypes()
+
+	is.NoError(err)
+	is.Len(fieldTypes, 1)
+	is.Equal(fieldTypes[0].Id, mockId)
+	is.Equal(fieldTypes[0].Name, mockName)
+	is.Equal(fieldTypes[0].ImageUrl, mockImageUrl)
+}
+
+func (suite *CourseTestSuite) TestGetAllFieldTypesWhenFailed() {
+	is := assert.New(suite.T())
+
+	mockCourseRepo := new(mockRepositories.CourseRepository)
+	mockFieldTypeRepo := new(mockRepositories.FieldTypeRepository)
+
+	mockFieldTypeRepo.On("FindAllFieldTypes").Return(nil, fmt.Errorf("fieldTypes not found"))
+	underTest := services.NewCourseService(mockCourseRepo, mockFieldTypeRepo)
+
+	fieldTypes, err := underTest.GetAllFieldTypes()
+
+	is.Nil(fieldTypes)
+	is.NotNil(err)
+
 }
 
 func TestCourseService(t *testing.T) {
