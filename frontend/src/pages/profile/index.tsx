32932d47a116937758ec02a70412f432bc8ccb6c
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2024-12-27 00:46:11 +0700

    handle error

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -5,33 +5,32 @@ import CourseCard from "../../components/coursecard"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Profile = () => {
-    const [profile, setProfile] = useState<PayloadProfile | null>(null)
-    const [enrolledCourses, setEnrolledCourses] = useState<PayloadEnrollwithCourse[]>([])
+    const [profile, setProfile] = useState<PayloadProfile | undefined>(undefined)
+    const [enrolledCourses, setEnrolledCourses] = useState<PayloadEnrollwithCourse[] | undefined>(undefined)
     const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                // Fetch profile data
                 const profileResponse = await server.profile.profileUserInfo()
-                if (profileResponse.data) {
+                if (profileResponse.code === 200) {
                     setProfile(profileResponse.data)
                 } else {
-                    setProfile(null)
-                    setError("Failed to fetch profile data.")
+                    setError("Failed to fetch profile data. Please try again.")
                     return
                 }
+
+                // Fetch enrolled courses
                 const coursesResponse = await server.courses.getEnrollCourseByUserId()
-                if (coursesResponse.data) {
+                if (coursesResponse.code !== 200) {
                     setEnrolledCourses(coursesResponse.data)
-                    if (coursesResponse.data.length === 0) {
-                        console.log("No courses found for this user")
-                    }
                 } else {
-                    setEnrolledCourses([])
+                    setError("Failed to fetch enrolled courses. Please try again.")
                 }
             } catch (error) {
                 console.error("Error during data fetching: ", error)
-                setError("An error occurred while fetching data")
+                setError("An error occurred while fetching data. Please try again.")
             }
         }
 
@@ -39,10 +38,14 @@ const Profile = () => {
     }, [])
 
     if (error) {
-        return <div>{error}</div>
-    }
-    if (!profile) {
-        return <div>Please Login first.</div>
+        return (
+            <div className="flex items-center justify-center w-screen">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold mb-4">{error}</h1>
+                    <p className="text-3xl text-gray-700">Please refresh the page and try again.</p>
+                </div>
+            </div>
+        )
     }
 
     const getFallBackName = (text: string) => {
@@ -55,27 +58,31 @@ const Profile = () => {
             <div className="flex flex-col items-start space-y-4">
                 <h1 className="text-3xl mt-10 mb-4 font-medium">Profile</h1>
                 <div className="flex flex-col sm:flex-row items-center  space-y-4 sm:space-x-6 sm:space-y-0">
-                        <Avatar className="w-28 h-28 sm:w-24 sm:h-24 rounded-full object-cover shadow-md bg-slate-200 text-continueCourse text-xl font-bold">
-                            <AvatarImage src={profile.photoUrl} alt={`${profile.firstname} ${profile.lastname}`} />
-                            <AvatarFallback>{getFallBackName(`${profile.firstname} ${profile.lastname}`)}</AvatarFallback>
-                        </Avatar>
-                    <div className="flex flex-col self-center">
-                        <h2 className="text-2xl sm:text-3xl font-medium">
-                            {profile.firstname} {profile.lastname}
-                        </h2>
-                        <p className="text-gray-500 text-sm sm:text-base">{profile.email}</p>
-                    </div>
+                    {profile && (
+                        <>
+                            <Avatar className="w-28 h-28 sm:w-24 sm:h-24 rounded-full object-cover shadow-md bg-slate-200 text-continueCourse text-xl font-bold">
+                                <AvatarImage src={profile.photoUrl} alt={`${profile.firstname} ${profile.lastname}`} />
+                                <AvatarFallback>{getFallBackName(`${profile.firstname} ${profile.lastname}`)}</AvatarFallback>
+                            </Avatar>
+                            <div className="flex flex-col self-center">
+                                <h2 className="text-2xl sm:text-3xl font-medium">
+                                    {profile.firstname} {profile.lastname}
+                                </h2>
+                                <p className="text-gray-500 text-sm sm:text-base">{profile.email}</p>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
 
             <div className="mt-10 sm:mt-20">
                 <h1 className="text-3xl font-medium">Enrolled Courses</h1>
                 <div className="mt-8 mb-10 gap-6 justify-center sm:justify-start">
-                    {enrolledCourses.length === 0 ? (
+                    {enrolledCourses?.length === 0 ? (
                         <p className="ml-10">No courses enrolled yet.</p>
                     ) : (
                         <div className="flex flex-wrap mt-8 mb-10 gap-6">
-                            {enrolledCourses.map((course) => (
+                            {enrolledCourses?.map((course) => (
                                 <CourseCard
                                     key={course.id}
                                     courseName={course.courseName?.name ?? ""}
